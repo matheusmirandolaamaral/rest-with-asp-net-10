a928c33ef269e7ab02806b1ae707ef3bf1cee68a
Matheus Mirandola do Amaral <matheusmirandola.amaral@gmail.com>
2026-02-04 15:50:22 -0300

    Add validation and column mapping to Person entity

--- RestWithAspNet10/RestWithAspNet10/Model/Person.cs
+++ RestWithAspNet10/RestWithAspNet10/Model/Person.cs
@@ -1,13 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RestWithAspNet10.Model
 {
+    [Table("person")]
     public class Person
     {
+        [Key]
+        [Column("id")]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+
+        [Required]
+        [Column("first_name", TypeName ="varchar(80)")]
+        [MaxLength(80)]
         public string FirstName { get; set; }
+
+
+        [Required]
+        [Column("last_name", TypeName = "varchar(80)")]
+        [MaxLength(80)]
         public string LastName { get; set; }
+
+
+        [Required]
+        [Column("address", TypeName = "varchar(100)")]
+        [MaxLength(100)]
         public string  Address { get; set; }
+
+
+        [Required]
+        [Column("gender", TypeName = "varchar(6)")]
+        [MaxLength(6)]
         public string Gender { get; set; }
     }
 }
