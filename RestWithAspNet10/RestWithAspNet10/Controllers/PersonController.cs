565ed9ab3b472d8ec630902a0f7be8da66bd4b31
Matheus Mirandola do Amaral <matheusmirandola.amaral@gmail.com>
2026-02-06 14:52:17 -0300

    Add logs to PersonController

--- RestWithAspNet10/RestWithAspNet10/Controllers/PersonController.cs
+++ RestWithAspNet10/RestWithAspNet10/Controllers/PersonController.cs
@@ -11,22 +11,28 @@ namespace RestWithAspNet10.Controllers
     public class PersonController : ControllerBase
     {
         private readonly IPersonService _personService;
-        public PersonController(IPersonService personService)
+        private readonly ILogger<PersonController> _logger;
+
+        public PersonController(IPersonService personService, ILogger<PersonController> logger)
         {
             _personService = personService;
+            _logger = logger;
         }
 
         [HttpGet]
         public IActionResult Get()
         {
+            _logger.LogInformation("Fetching all persons");
             return Ok(_personService.FindAll());
         }
         [HttpGet("{id}")]
         public IActionResult Get(long id)
         {
+            _logger.LogInformation("Fetching persons with ID {id}", id);
             var person = _personService.FindById(id);
             if(person == null)
             {
+                _logger.LogWarning("Person with ID {id} not found", id);
                 return NotFound();
             }
             return Ok(person);
@@ -35,9 +41,11 @@ namespace RestWithAspNet10.Controllers
         [HttpPost]
         public IActionResult Post([FromBody] Person person)
         {
+            _logger.LogInformation("Creating new Person: {firstName}",person.FirstName );
             var createPerson = _personService.Create(person);
             if(createPerson == null)
             {
+                _logger.LogError("Failed to create person with name {firstName}", person.FirstName);
                 return NotFound();
             }
             return Ok(createPerson);
@@ -47,18 +55,23 @@ namespace RestWithAspNet10.Controllers
         [HttpPut]
         public IActionResult Put([FromBody] Person person)
         {
+            _logger.LogInformation("Updating persons with ID {id}", person.Id);
             var updatePerson = _personService.Update(person);
             if (updatePerson == null)
             {
+                _logger.LogError("Failed to update person with ID {id}", person.Id);
                 return NotFound();
             }
+            _logger.LogDebug("Person update sucessfully: {firstName}", updatePerson.FirstName);
             return Ok(updatePerson);
         }
 
         [HttpDelete("{id}")]
         public IActionResult Delete(long id)
         {
+            _logger.LogInformation("Deleting persons with ID {id}", id);
             _personService.Delete(id);
+            _logger.LogDebug("Person with ID {id} deleted sucessfully", id);
             return NoContent();
         }
 
