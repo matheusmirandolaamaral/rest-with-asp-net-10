cb9ae34ce6744b42e517e333f669a8972890eedc
Matheus Mirandola do Amaral <matheusmirandola.amaral@gmail.com>
2026-02-10 16:02:07 -0300

    Create books repository

---
+++ RestWithAspNet10/RestWithAspNet10/Repository/IBookRepository.cs
@@ -0,0 +1,13 @@
+﻿using RestWithAspNet10.Model;
+
+namespace RestWithAspNet10.Repository
+{
+    public interface IBookRepository
+    {
+        Book Create(Book book);
+        Book? FindById(long id);
+        List<Book> FindAll();
+        Book Update(Book book);
+        void Delete(long id);
+    }
+}
