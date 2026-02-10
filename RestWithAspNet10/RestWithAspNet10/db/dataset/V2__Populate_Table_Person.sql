b28a05ca58253be539a41ecfbf6076883405fb78
Matheus Mirandola do Amaral <matheusmirandola.amaral@gmail.com>
2026-02-10 14:19:51 -0300

    Create dataset and rename first migration

---
+++ RestWithAspNet10/RestWithAspNet10/db/dataset/V2__Populate_Table_Person.sql
@@ -0,0 +1,7 @@
+
+INSERT [dbo].[person] ([first_name], [last_name], [address], [gender]) VALUES (N'Neymar', N'Jr', N'Santos - Brasil', N'Male');
+
+INSERT [dbo].[person] ([first_name], [last_name], [address], [gender]) VALUES (N'Lionel', N'Messi', N'Argentina - Buenos Aires', N'Male');
+
+INSERT [dbo].[person] ([first_name], [last_name], [address], [gender]) VALUES (N'Ronaldo', N'Fenomeno', N'Brasil', N'Male');
+
