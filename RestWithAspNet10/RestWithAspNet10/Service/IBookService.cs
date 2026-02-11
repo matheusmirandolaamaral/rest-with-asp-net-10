9c4744a7194ebc2d720e3e10637bf3ee29542a2a
Matheus Mirandola do Amaral <matheusmirandola.amaral@gmail.com>
2026-02-11 14:43:31 -0300

    Create books service

---
+++ RestWithAspNet10/RestWithAspNet10/Service/IBookService.cs
@@ -0,0 +1,13 @@
+﻿using RestWithAspNet10.Model;
+
+namespace RestWithAspNet10.Service
+{
+    public interface IBookService
+    {
+        Book Create(Book book);
+        Book? FindById(long id);
+        List<Book> FindAll();
+        Book Update(Book book);
+        void Delete(long id);
+    }
+}
---
+++ RestWithAspNet10/RestWithAspNet10/Service/Impl/BookServiceImpl.cs
@@ -0,0 +1,41 @@
+﻿using RestWithAspNet10.Model;
+using RestWithAspNet10.Repository;
+
+namespace RestWithAspNet10.Service.Impl
+{
+    public class BookServiceImpl : IBookService
+    {
+        private IBookRepository _repository;
+        public BookServiceImpl(IBookRepository repository)
+        {
+            _repository = repository;
+        }
+
+
+        public List<Book> FindAll()
+        {
+            return _repository.FindAll();
+        }
+
+        public Book? FindById(long id)
+        {
+            return _repository.FindById(id);
+        }
+
+
+        public Book Create(Book book)
+        {
+            return _repository.Create(book);
+        }
+        public Book Update(Book book)
+        {
+           return _repository.Update(book);
+        }
+
+        public void Delete(long id)
+        {
+            _repository.Delete(id);
+        }
+
+    }
+}
