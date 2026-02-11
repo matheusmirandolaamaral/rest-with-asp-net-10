5ed052d7a34a2b423a6132898fb5b784c41d61f7
Matheus Mirandola do Amaral <matheusmirandola.amaral@gmail.com>
2026-02-11 15:26:27 -0300

    Create BookController

---
+++ RestWithAspNet10/RestWithAspNet10/Controllers/BookController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestWithAspNet10.Model;
+using RestWithAspNet10.Service;
+
+
+namespace RestWithAspNet10.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookController : ControllerBase
+    {
+        private readonly IBookService _bookService;
+        private readonly ILogger<BookController> _logger;
+        public BookController(IBookService bookService, ILogger<BookController> logger)
+        {
+            _bookService = bookService;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _logger.LogInformation("Fetching all books");
+            return Ok(_bookService.FindAll());
+        }
+        [HttpGet]
+        public IActionResult Get(long id)
+        {
+            _logger.LogInformation("Fetching books with ID {id}", id);
+            var book = _bookService.FindById(id);
+            if(book == null)
+            {
+                _logger.LogWarning("Book with ID {id} not found", id);
+                return NotFound();
+            }
+            return Ok(book);
+        }
+
+
+        [HttpPost]
+        public IActionResult Post(Book book)
+        {
+            _logger.LogInformation("Creating book");
+            var books = _bookService.Create(book);
+            if(books == null)
+            {
+                _logger.LogError("Failed to create book");
+                return NotFound();
+            }
+            return Ok(books);
+        }
+
+        [HttpPut]
+        public IActionResult Put(Book book)
+        {
+            _logger.LogInformation("Updating book with ID {id}", book.Id);
+            var updatebook = _bookService.Update(book);
+            if(updatebook == null)
+            {
+                _logger.LogError("Failed to update book with ID {id}", book.Id);
+                return NotFound();
+            }
+            return Ok(updatebook);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(long id)
+        {
+            _logger.LogInformation("Deleting book with ID {id}", id);
+            _bookService.Delete(id);
+            _logger.LogDebug("Book with ID {id} deleted sucessfully", id);
+            return NoContent();
+        }
+    }
+
+    
+}
