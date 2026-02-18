83001cbda75ee82f39bcdee7b34ec7baec820afa
Matheus Mirandola do Amaral <matheusmirandola.amaral@gmail.com>
2026-02-18 16:19:55 -0300

    Add xUnit tests

---
+++ RestWithAspNet10/RestWithAspNet10.Tests/PersonConverterTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RestWithAspNet10.Tests
+{
+    public class PersonConverterTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
