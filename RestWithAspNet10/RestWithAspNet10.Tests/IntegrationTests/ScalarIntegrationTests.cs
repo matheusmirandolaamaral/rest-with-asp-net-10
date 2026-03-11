8fc89b798e69d228760cf89ed185f7516e7f6d7b
Matheus Mirandola do Amaral <matheusmirandola.amaral@gmail.com>
2026-03-11 17:44:48 -0300

    Add Scalar Tests

---
+++ RestWithAspNet10/RestWithAspNet10.Tests/IntegrationTests/ScalarIntegrationTests.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using RestWithAspNet10.Tests.IntegrationTests.Tools;
+
+namespace RestWithAspNet10.Tests.IntegrationTests
+{
+    public class ScalarIntegrationTests : IClassFixture<SqlServerFixture>
+    {
+        private readonly HttpClient _httpClient;
+
+        public ScalarIntegrationTests(SqlServerFixture sqlFixture)
+        {
+            var factory = new CustomWebApplicationFactory<Program>(sqlFixture.ConnectionString);
+            _httpClient = factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                BaseAddress = new Uri("http://localhost")
+            });
+        }
+
+
+        [Fact]
+        public async Task Scalar_ShouldReturnScalarUI()
+        {
+            // Arrange & Act
+            var response = await _httpClient.GetAsync("/scalar/");
+
+            //Assert
+            response.EnsureSuccessStatusCode();
+            var content = await response.Content.ReadAsStringAsync();
+            content.Should().Contain("<title>ASP.NET 2026 REST API's from 0 to Azure and GCP with .NET 10, Docker e Kubernetes</title>");
+            content.Should().Contain("script src");
+        }
+    }
+}
