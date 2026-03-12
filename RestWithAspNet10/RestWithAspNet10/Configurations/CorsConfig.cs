ce3f682522692e78b6bd38dc002cd446fa386a4e
Matheus Mirandola do Amaral <matheusmirandola.amaral@gmail.com>
2026-03-11 22:40:13 -0300

    Update CorsConfig

--- RestWithAspNet10/RestWithAspNet10/Configurations/CorsConfig.cs
+++ RestWithAspNet10/RestWithAspNet10/Configurations/CorsConfig.cs
@@ -2,6 +2,24 @@
 {
     public static class CorsConfig
     {
-        
+        public static void AddCorsConfinguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("LocalPolicy", policy =>
+                {
+                    policy.WithOrigins("http://localhost:3000")
+                           .AllowAnyMethod()
+                           .AllowAnyHeader()
+                           .AllowCredentials();
+                });
+            });
+        }
+        public static IApplicationBuilder UseCorsConfiguration(this WebApplication app)
+        {
+            app.UseCors();
+            return app;
+
+        }
     }
 }
