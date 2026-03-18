c98759411063fc7f85c8f467f52b481d3c392fb5
Matheus Mirandola do Amaral <matheusmirandola.amaral@gmail.com>
2026-03-18 18:49:43 -0300

    Add HATEOAS configuration

---
+++ RestWithAspNet10/RestWithAspNet10/Configurations/HATEOASConfig.cs
@@ -0,0 +1,22 @@
+﻿using RestWithAspNet10.Hypermedia.Enricher;
+using RestWithAspNet10.Hypermedia.Filters;
+
+namespace RestWithAspNet10.Configurations
+{
+    public static class HATEOASConfig
+    {
+        public static IServiceCollection AddHATEOASConfiguration(this IServiceCollection services)
+        {
+            var filterOptions = new HypermediaFilterOptions();
+            filterOptions.ContentResponseEnricherList.Add(new PersonEnricher());
+            services.AddSingleton(filterOptions);
+
+            services.AddScoped<HypermediaFilter>();
+            return services;
+        }
+
+        public static void UseHATEOASRoutes(this IEndpointRouteBuilder app)
+        {
+            app.MapControllerRoute("Default", "{controller=values}/v1/{id?}");
+        }
+}
--- RestWithAspNet10/RestWithAspNet10/Program.cs
+++ RestWithAspNet10/RestWithAspNet10/Program.cs
@@ -1,4 +1,5 @@
 using RestWithAspNet10.Configurations;
+using RestWithAspNet10.Hypermedia.Filters;
 using RestWithAspNet10.Repository;
 using RestWithAspNet10.Repository.Impl;
 using RestWithAspNet10.Service;
@@ -10,7 +11,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.AddSerilogLogging();
 // Add services to the container.
 
-builder.Services.AddControllers().AddContentNegotiation();
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<HypermediaFilter>();
+}).AddContentNegotiation();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenAPIConfig();
@@ -18,6 +22,7 @@ builder.Services.AddSwaggerConfig();
 builder.Services.AddRouteConfig();
 
 builder.Services.AddCorsConfinguration(builder.Configuration);
+builder.Services.AddHATEOASConfiguration();
 
 builder.Services.AddDatabaseConfiguration(builder.Configuration);
 builder.Services.AddEvolveConfiguration(builder.Configuration,builder.Environment);
@@ -42,6 +47,8 @@ app.UseRouting();
 app.UseCorsConfiguration(builder.Configuration);
 
 app.MapControllers();
+
+app.UseHATEOASRoutes();
 
 app.UseSwaggerSpecification();
 app.UseScalarSpecification();
