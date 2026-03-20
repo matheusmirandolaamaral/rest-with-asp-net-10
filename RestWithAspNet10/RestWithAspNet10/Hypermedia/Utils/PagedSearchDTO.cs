7e6e27afb542701bd2c7057988c4e79437088b15
Matheus Mirandola do Amaral <matheusmirandola.amaral@gmail.com>
2026-03-20 18:58:10 -0300

    feat: add PagedSearchDTO

---
+++ RestWithAspNet10/RestWithAspNet10/Hypermedia/Utils/PagedSearchDTO.cs
@@ -0,0 +1,29 @@
+﻿using RestWithAspNet10.Hypermedia.Abstract;
+
+namespace RestWithAspNet10.Hypermedia.Utils
+{
+    public class PagedSearchDTO<T> where T : ISupportsHypermedia
+    {
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalResults { get; set; }
+        public string SortFields { get; set; }
+        public string SortDirection { get; set; } = "asc";
+        public Dictionary<string, object> Filters { get; set; } = [];
+        public List<T> List { get; set; } = [];
+
+        public PagedSearchDTO() { }
+        public PagedSearchDTO(int currentPage, int pageSize,  string sortFields, string sortDirection, Dictionary<string, object> filters)
+        {
+            CurrentPage = currentPage;
+            PageSize = pageSize;
+            SortFields = sortFields;
+            SortDirection = sortDirection;
+            Filters = filters ?? [];
+        }
+        public PagedSearchDTO(int currentPage,  string sortFields, string sortDirection): this (currentPage, 10, sortFields, sortDirection, null) { }
+        
+        public int GetCurrentPage() => CurrentPage == 0 ? 1 : CurrentPage;
+        public int GetPageSize() => PageSize == 0 ? 10 : PageSize;
+    }
+}
