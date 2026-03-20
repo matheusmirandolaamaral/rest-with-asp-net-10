e4e045a376cbed7ec786b779ccbd2234f267dc1d
Matheus Mirandola do Amaral <matheusmirandola.amaral@gmail.com>
2026-03-20 01:49:57 -0300

    Update PersonRepository

--- RestWithAspNet10/RestWithAspNet10/Repository/IPersonRepository.cs
+++ RestWithAspNet10/RestWithAspNet10/Repository/IPersonRepository.cs
@@ -5,5 +5,6 @@ namespace RestWithAspNet10.Repository
     public interface IPersonRepository : IRepository<Person>
     {
         Person? Disable(long id);
+        List<Person> FindByName(string firstName, string lastName);
     }
 }
--- RestWithAspNet10/RestWithAspNet10/Repository/Impl/PersonRepository.cs
+++ RestWithAspNet10/RestWithAspNet10/Repository/Impl/PersonRepository.cs
@@ -13,5 +13,18 @@ namespace RestWithAspNet10.Repository.Impl
             _context.SaveChanges();
             return person;
         }
+
+        public List<Person> FindByName(string firstName, string lastName)
+        {
+            var query = _context.Persons.AsQueryable();
+            if(!string.IsNullOrWhiteSpace(firstName))
+                query = query.Where(p => p.FirstName.Contains(firstName));
+
+            if(!string.IsNullOrWhiteSpace(lastName))
+                query = query.Where(p => p.LastName.Contains(lastName));
+
+           // return query.ToList();
+            return [.. query]; // C# 14.0 collection expression syntax, equivalent to query.ToList()
+        }
     }
 }
