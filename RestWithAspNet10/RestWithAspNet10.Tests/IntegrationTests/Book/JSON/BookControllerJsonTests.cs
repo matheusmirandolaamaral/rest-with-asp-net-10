6a6729047d1885c1b9efb141946272418a9b04bf
Matheus Mirandola do Amaral <matheusmirandola.amaral@gmail.com>
2026-03-26 01:52:14 -0300

    test: add JSON integration tests for BookController

---
+++ RestWithAspNet10/RestWithAspNet10.Tests/IntegrationTests/Book/JSON/BookControllerJsonTests.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using RestWithAspNet10.Data.DTO.V1;
+using RestWithAspNet10.Tests.IntegrationTests.Tools;
+using System.Net.Http.Json;
+
+namespace RestWithAspNet10.Tests.IntegrationTests.Book.JSON
+{
+    [TestCaseOrderer(TestConfigs.TestCaseOrdererFullName, TestConfigs.TestCaseOrdererAssembly)]
+    public class BookControllerJsonTests : IClassFixture<SqlServerFixture>
+    {
+        private readonly HttpClient _httpClient;
+        private static BookDTO _book;
+
+        public BookControllerJsonTests(SqlServerFixture fixture)
+        {
+            var factory = new CustomWebApplicationFactory<Program>(fixture.ConnectionString);
+            _httpClient = factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                BaseAddress = new Uri("http://localhost")
+            });
+        }
+
+        [Fact(DisplayName = "01 - Created Book")]
+        [TestPriority(1)]
+        public async Task CreateBook_ShouldReturnCreatedBook()
+        {
+            // Arrange
+            var request = new BookDTO
+            {
+                Title = "Rest with ASP.NET Core 10",
+                Author = "Lamine Yamal",
+                Price = 49.99M,
+                LaunchDate = new DateTime(1925, 4, 10)
+            };
+            // Act
+            var response = await _httpClient.PostAsJsonAsync("api/book/v1", request);
+            //Assert
+            response.EnsureSuccessStatusCode();
+            var created = await response.Content.ReadFromJsonAsync<BookDTO>();
+            created.Should().NotBeNull();
+            created.Id.Should().BeGreaterThan(0);
+            created.Title.Should().Be("Rest with ASP.NET Core 10");
+            created.Author.Should().Be("Lamine Yamal");
+            created.Price.Should().Be(49.99M);
+            _book = created;
+
+
+        }
+    }
+}
