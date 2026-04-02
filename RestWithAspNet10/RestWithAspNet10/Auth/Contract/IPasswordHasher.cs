5a1b4fde1e6f317cda206a2cb205f3a6e9d81f71
Matheus Mirandola do Amaral <matheusmirandola.amaral@gmail.com>
2026-04-02 18:44:15 -0300

    feat: add SHA-256 password hashing and verification utilities

---
+++ RestWithAspNet10/RestWithAspNet10/Auth/Contract/IPasswordHasher.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection.Metadata;
+
+namespace RestWithAspNet10.Auth.Contract
+{
+    public interface IPasswordHasher
+    {
+        string Hash(string password);
+        bool Verify(string password, string hashedPassword);
+    }
+}
---
+++ RestWithAspNet10/RestWithAspNet10/Auth/Tools/Sha256PasswordHasher.cs
@@ -0,0 +1,27 @@
+﻿using RestWithAspNet10.Auth.Contract;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RestWithAspNet10.Auth.Tools
+{
+    public class Sha256PasswordHasher : IPasswordHasher
+    {
+        public string Hash(string password)
+        {
+            var inputBytes = Encoding.UTF8.GetBytes(password);
+            var hashBytes =SHA256.HashData(inputBytes);
+
+            var builder = new StringBuilder();
+            foreach (var b in hashBytes)
+            {
+                builder.Append(b.ToString("x2"));
+            }
+            return builder.ToString();
+        }
+
+        public bool Verify(string password, string hashedPassword)
+        {
+            return Hash(password) == hashedPassword;
+        }
+    }
+}
