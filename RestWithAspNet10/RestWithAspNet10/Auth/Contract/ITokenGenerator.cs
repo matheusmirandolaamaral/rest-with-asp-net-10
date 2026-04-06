318374b25f5942b453c8a9bf23710e430b9a8694
Matheus Mirandola do Amaral <matheusmirandola.amaral@gmail.com>
2026-04-06 16:50:20 -0300

    feat: add utility classes for authentication token generation

---
+++ RestWithAspNet10/RestWithAspNet10/Auth/Contract/ITokenGenerator.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+
+namespace RestWithAspNet10.Auth.Contract
+{
+    public interface ITokenGenerator
+    {
+        string GenerateAccessToken(IEnumerable<Claim> claims);
+        string GenerateRefreshToken();
+        ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
+    }
+}
---
+++ RestWithAspNet10/RestWithAspNet10/Auth/Tools/TokenGenerator.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using RestWithAspNet10.Auth.Config;
+using RestWithAspNet10.Auth.Contract;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace RestWithAspNet10.Auth.Tools
+{
+    public class TokenGenerator : ITokenGenerator
+    {
+        private readonly TokenConfiguration _configurations;
+        public TokenGenerator(TokenConfiguration configurations)
+        {
+            _configurations = configurations;
+        }
+
+        public string GenerateAccessToken(IEnumerable<Claim> claims)
+        {
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configurations.Secret));
+
+            var signingCredentials = new SigningCredentials(secretKey,SecurityAlgorithms.HmacSha256);
+
+            var tokenOptions = new JwtSecurityToken(
+                issuer: _configurations.Issuer,
+                audience: _configurations.Audience,
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(_configurations.Minutes),
+                signingCredentials: signingCredentials
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+        }
+
+        public string GenerateRefreshToken()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+        {
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configurations.Secret)),
+                ValidateLifetime = false
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out var securityToken);
+
+            if(securityToken is not JwtSecurityToken jwtSecurityToken || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+            {
+                throw new SecurityTokenException("Invalid token");
+            }
+
+            return principal;
+        }
+    }
+}
