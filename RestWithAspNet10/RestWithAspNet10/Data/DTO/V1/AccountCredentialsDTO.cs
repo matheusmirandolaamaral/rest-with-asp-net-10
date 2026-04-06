21a5ebed6d25617c75d917686efacd5c20c31a90
Matheus Mirandola do Amaral <matheusmirandola.amaral@gmail.com>
2026-04-06 15:03:08 -0300

    feat: introduce input and output DTOs for authentication endpoint

---
+++ RestWithAspNet10/RestWithAspNet10/Data/DTO/V1/AccountCredentialsDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace RestWithAspNet10.Data.DTO.V1
+{
+    public class AccountCredentialsDTO
+    {
+        public AccountCredentialsDTO() { }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Fullname { get; set; }
+    }
+}
---
+++ RestWithAspNet10/RestWithAspNet10/Data/DTO/V1/UserDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace RestWithAspNet10.Data.DTO.V1
+{
+    public class UserDTO
+    {
+        public UserDTO() { }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
