0550f86525531951384b18cb7edbdb56c8d4a53c
Matheus Mirandola do Amaral <matheusmirandola.amaral@gmail.com>
2026-04-07 17:10:42 -0300

    feat: implement API login service

---
+++ RestWithAspNet10/RestWithAspNet10/Service/ILoginService.cs
@@ -0,0 +1,12 @@
+﻿using RestWithAspNet10.Data.DTO.V1;
+
+namespace RestWithAspNet10.Service
+{
+    public interface ILoginService
+    {
+        TokenDTO? ValidateCredentials(UserDTO user);
+        TokenDTO? ValidateCredentials(TokenDTO token);
+        bool RevokeToken(string username);
+        AccountCredentialsDTO Create(AccountCredentialsDTO user);
+    }
+}
---
+++ RestWithAspNet10/RestWithAspNet10/Service/Impl/LoginServiceImpl.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.IdentityModel.JsonWebTokens;
+using RestWithAspNet10.Auth.Config;
+using RestWithAspNet10.Auth.Contract;
+using RestWithAspNet10.Data.DTO.V1;
+using RestWithAspNet10.Model;
+using System.Security.Claims;
+
+namespace RestWithAspNet10.Service.Impl
+{
+    public class LoginServiceImpl : ILoginService
+    {
+        private const string DATE_FORMAT = "yyyy-MM-dd HH:mm:ss";
+
+        private readonly IUserAuthService _userAuthService;
+        private readonly IPasswordHasher _passwordHasher;
+        private readonly ITokenGenerator _tokenService;
+        private readonly TokenConfiguration _configurations;
+
+        public LoginServiceImpl(IUserAuthService userAuthService, IPasswordHasher passwordHasher, ITokenGenerator tokenService, TokenConfiguration configurations)
+        {
+            _userAuthService = userAuthService;
+            _passwordHasher = passwordHasher;
+            _tokenService = tokenService;
+            _configurations = configurations;
+        }
+
+        public TokenDTO? ValidateCredentials(UserDTO userDto)
+        {
+            var user = _userAuthService.FindByUsername(userDto.Username);
+
+            if(user == null) return null;
+
+            if(!_passwordHasher.Verify(userDto.Password,user.Password)) return null;
+
+            return GenerateToken(user);
+        }
+
+
+        public TokenDTO? ValidateCredentials(TokenDTO token)
+        {
+            throw new NotImplementedException();
+        }
+        public AccountCredentialsDTO Create(AccountCredentialsDTO user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RevokeToken(string username)
+        {
+            throw new NotImplementedException();
+        }
+        private TokenDTO GenerateToken(User user, IEnumerable<Claim>? existingClaims = null)
+        {
+            var claims = existingClaims?.ToList() ?? new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString("N")),
+                new Claim(JwtRegisteredClaimNames.UniqueName, user.Username)
+            };
+
+            var accessToken = _tokenService.GenerateAccessToken(claims);
+
+            var refreshToken = _tokenService.GenerateRefreshToken();
+
+            user.RefreshToken = refreshToken;
+            user.RefreshTokenExpiryTime = DateTime.Now.AddDays(_configurations.DaysToExpiry);
+
+            _userAuthService.Update(user);
+
+            var createdDate = DateTime.Now;
+            var expirationDate = createdDate.AddMinutes(_configurations.Minutes);
+
+            return new TokenDTO
+            {
+                Authenticated = true,
+                Created = createdDate.ToString(DATE_FORMAT),
+                Expiration = expirationDate.ToString(DATE_FORMAT),
+                AccessToken = accessToken,
+                RefreshToken = refreshToken
+            };
+        }
+
+    }
+}
