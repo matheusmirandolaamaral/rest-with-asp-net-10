8cf2071fab2b3912fec8d3726c5d74b853641cf4
Matheus Mirandola do Amaral <matheusmirandola.amaral@gmail.com>
2026-04-07 15:05:34 -0300

    feat: implement user authentication service

---
+++ RestWithAspNet10/RestWithAspNet10/Service/IUserAuthService.cs
@@ -0,0 +1,13 @@
+﻿using RestWithAspNet10.Data.DTO.V1;
+using RestWithAspNet10.Model;
+
+namespace RestWithAspNet10.Service
+{
+    public interface IUserAuthService
+    {
+        User? FindByUsername(string username);
+        User Create(AccountCredentialsDTO dto); // usado somente para recurso didatico
+        bool RevokeToken(string username);
+        User Update(User user);
+    }
+}
---
+++ RestWithAspNet10/RestWithAspNet10/Service/Impl/UserAuthServiceImpl.cs
@@ -0,0 +1,55 @@
+﻿using RestWithAspNet10.Auth.Contract;
+using RestWithAspNet10.Data.DTO.V1;
+using RestWithAspNet10.Model;
+using RestWithAspNet10.Repository;
+
+namespace RestWithAspNet10.Service.Impl
+{
+    public class UserAuthServiceImpl : IUserAuthService
+    {
+        private readonly IUserRepository _repository;
+        private readonly IPasswordHasher _passwordHasher;
+
+        public UserAuthServiceImpl(IUserRepository repository, IPasswordHasher passwordHasher)
+        {
+            _repository = repository;
+            _passwordHasher = passwordHasher;
+        }
+
+        public User? FindByUsername(string username)
+        {
+           return _repository.FindByUserName(username);
+        }
+
+
+        // usado somente para recurso didatico, pois isso é considerado uma falha de segurancaa 
+        public User Create(AccountCredentialsDTO dto)
+        {
+            if(dto == null) throw new ArgumentNullException(nameof(dto));
+
+            var entity = new User
+            {
+                Username = dto.Username,
+                FullName = dto.Fullname,
+                Password = _passwordHasher.Hash(dto.Password),
+                RefreshToken = string.Empty,
+                RefreshTokenExpiryTime = null
+            };
+            return _repository.Create(entity);
+        }
+
+        public bool RevokeToken(string username)
+        {
+            var user = _repository.FindByUserName(username);
+            if(user == null) return false;
+            user.RefreshToken = null;
+            _repository.Update(user);
+            return true;
+        }
+
+        public User Update(User user)
+        {
+            return _repository.Update(user);
+        }
+    }
+}
